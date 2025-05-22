c76e2e68fb180b8584375130bd0f1f98d40eaed6
arzumify <jliwin98@danwin1210.de>
2025-05-22 19:39:51 +0100

    See HISTORY.md in the Octuple repository: 15052025

--- lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod request;
-pub mod room;
-pub mod serde;
-pub mod server;
-pub mod space;
-pub mod user;
-pub mod message;
-pub mod endpoints;
-pub mod error;
--- serde/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod signature_bytes;
-pub mod uuid;
